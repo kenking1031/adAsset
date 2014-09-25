0e9df052ebc7287e4b815117c6e072d2ebd9b942
Lixiang Jiang <ljian001@fiu.edu>
2014-09-24 18:05:24 -0700

    Use unicorn as rails server

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,16 @@ module AdAssets
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.initialize_on_precompile = true
+
+    # Configure generators
+    config.generators do |g|
+      g.test_framework :rspec
+      g.view_specs false
+      g.helper false
+      g.helper_specs false
+      g.assets false
+    end
   end
 end
+
