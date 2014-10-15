3c9bdec7c0971bc1d7552b41fa68d84d429bb2dd
Lixiang Jiang <ljian001@fiu.edu>
2014-10-15 15:39:29 -0700

    Implement angularjs framework

--- app/controllers/api/channels_controller.rb
+++ app/controllers/api/channels_controller.rb
@@ -6,32 +6,40 @@ class Api::ChannelsController < ApplicationController
 
     @message = "Watchable Ad Assets"
 
-    @channel_videos = Finder.find($MidTierToken, nil, nil, nil, "channels", nil, nil, nil, nil, size, nil, "/api/web/admin/search/channels/"+SiteConfig.CHANNEL_ID+"/videos")[0]
+    channel_videos_response = Finder.find($MidTierToken, nil, nil, nil, "channels", nil, nil, nil, nil, size, nil, "/api/web/admin/search/channels/#{SiteConfig.CHANNEL_ID}/videos")
+
+    @channel_videos =channel_videos_response[0]
 
     @data = []
 
-    @channel_videos.items.each do |video|
+    if channel_videos_response[2] ==200
+      @channel_videos.items.each do |video|
 
-      # videoDetail = Finder.find($MidTierToken, nil, nil, nil, "videos", nil, nil, nil, nil, nil, nil, "/api/web/admin/search/videos/"+video['id'])[0]
-      # title = videoDetail.title
-      # created_time = videoDetail.live_broad_cast_time
-      thumbnail = video['links']['cell-image']
-      title = video['title']
-      created_time = video['liveBroadcastTime']
+        # videoDetail = Finder.find($MidTierToken, nil, nil, nil, "videos", nil, nil, nil, nil, nil, nil, "/api/web/admin/search/videos/"+video['id'])[0]
+        # title = videoDetail.title
+        # created_time = videoDetail.live_broad_cast_time
+        thumbnail = video['links']['cell-image']
+        title = video['title']
+        created_time = video['liveBroadcastTime']
 
-      playbackItem = Finder.find($MidTierToken, nil, nil, nil, "videos", nil, nil, nil, nil, nil, nil, "/api/web/admin/search/videos/"+video['id']+"/playbackuri?videoFormat=MP4")[0]
-      if playbackItem.contentURL.blank?
-        play_url = "error"
-      else
-        play_url = playbackItem.contentURL
+        playbackItem = Finder.find($MidTierToken, nil, nil, nil, "videos", nil, nil, nil, nil, nil, nil, "/api/web/admin/search/videos/#{video['id']}/playbackuri?videoFormat=MP4")[0]
+        if playbackItem.contentURL.blank?
+          play_url = "error"
+        else
+          play_url = playbackItem.contentURL
+        end
+
+        @data << {"thumbnail" => thumbnail, "title" => title, "created_time" => created_time, "play_url" => play_url}
+
       end
 
-      @data << {"thumbnail" => thumbnail, "title" => title, "created_time" => created_time, "play_url" => play_url}
+      result={:status => channel_videos_response[2], :assets => @data, :size => size, :message => @message , :channelId => SiteConfig.CHANNEL_ID, :total_assets => @channel_videos.no_of_videos }
+    else
+
+      result={:status => channel_videos_response[2]}
 
     end
-
-    result={:assets => @data, :size => size, :message => @message , :channelId => SiteConfig.CHANNEL_ID, :total_assets => @channel_videos.no_of_videos }
-
+      Rails.logger.info(result);
     render json: result, status: :ok
 
   end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,35 +1,7 @@
 class HomeController < ApplicationController
 
   def index
-
     @message = "Watchable Ad Assets"
-
-    @channel_videos = Finder.find($MidTierToken, nil, nil, nil, "channels", nil, nil, nil, nil, nil, nil, "/api/web/admin/search/channels/"+SiteConfig.CHANNEL_ID+"/videos")[0]
-
-    @data = []
-
-    @channel_videos.items.each do |video|
-
-      # videoDetail = Finder.find($MidTierToken, nil, nil, nil, "videos", nil, nil, nil, nil, nil, nil, "/api/web/admin/search/videos/"+video['id'])[0]
-      # title = videoDetail.title
-      # created_time = videoDetail.live_broad_cast_time
-      thumbnail = video['links']['cell-image']
-      title = video['title']
-      created_time = video['liveBroadcastTime']
-
-      playbackItem = Finder.find($MidTierToken, nil, nil, nil, "videos", nil, nil, nil, nil, nil, nil, "/api/web/admin/search/videos/"+video['id']+"/playbackuri?videoFormat=MP4")[0]
-      if playbackItem.contentURL.blank?
-        play_url = "error"
-      else
-        play_url = playbackItem.contentURL
-      end
-
-      @data << {"thumbnail" => thumbnail, "title" => title, "created_time" => created_time, "play_url" => play_url}
-
-    end
-
-    @data
-
   end
 
 end
